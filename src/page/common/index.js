274fbf4e8657e1e5ace703c52c379d297c2294e5
舒云风 <shuyunfengcsdn@gmail.com>
2018-06-03 23:44:33 +0800

    initial

---
+++ src/page/common/index.js
@@ -0,0 +1,5 @@
+/**
+ * @author Huanyu
+ * @date 2018/5/27
+ */
+console.log('i am global');
---
+++ src/page/index/index.js
@@ -0,0 +1,6 @@
+// console.log('hello index');
+// var $$ = require('jquery');
+// $$('body').html('hello index ~~~~~');
+
+require('../module.js');
+require('./index.css');
